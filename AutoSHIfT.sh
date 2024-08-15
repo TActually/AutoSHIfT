5e2ce80ffe2b657f6c9017cb3db6718cabc23550
T Actually <tactually@outlook.com>
2024-08-15 08:33:19 -0700

    Update AutoSHIfT.sh
    Updating to v1.2 to match binary uploaded from my system. See release for changes.

--- AutoSHIfT.sh
+++ AutoSHIfT.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#! /usr/bin/bash
 
-#  Welcome to AutoSHIfT.
+#  Welcome to AutoSHIfT v1.2
 #  Author: T Actually tactually@outlook.com
 #  Website: https://github.com/TActually/AutoSHIfT
 #  Licensed under GNU General Public License v3.0
@@ -15,13 +15,13 @@
 #  ***IMPORTANT*** First/Manual run of this script must be run as root (sudo)!
 ################################################################################
 # check for root priviledges
-if [ $(id -u) -ne 0 ]
-  then echo "Please Run this script as root or with sudo."
+if [ "$(id -u)" -ne 0 ]
+  then echo "Please run this script as root or with sudo."
   exit
 fi
 
 # This line discovers the 1st signed in user for the location of the script & logs.
-liuser=$(users | cut -d ' ' -f 1)
+liuser=$(users | cut -d ' ' -f 2)
 #liuser=USERNAME  #uncomment this line, comment the line above it to manually enter user.
 
 # IMPORTANT: All timeshift backups (manual or automated) are counted and can be removed.
@@ -35,65 +35,67 @@ TSCount=$(timeshift --list | wc -l)
 TSList=21
 
 # This line controls the number of days that logs are kept before being purged.
-D2K=60
+D2K=31
 
 # This section will check to make sure that Timeshift is installed
 # If Not, script will install timeshift, then exit(quit).
 # The script exits to allow you to configure timeshift before creating your first backup.
-if [ $(dnf list installed "timeshift" | grep -c "timeshift") -ge 1 ]
+if [ "$(dnf list installed "timeshift" | grep -c "timeshift")" -ge 1 ]
 then
     :
 else
-    echo "This script requires Timeshift backup utility and it ain't here. Installing it..." && sleep 1s;
+    echo "This script requires Timeshift backup utility and it ain't here. Installing it..." && sleep 2s;
     echo "Now!" && sleep 1s; dnf install timeshift -y;
-    echo "Now that Timeshift has been installed, please take the time to set it up.";
-    echo "You'll need to setup your preferred backup method, selected folders and backup storage location!" && sleep 2s;
-    echo "Then, re-run this script!"
+    echo "Now that Timeshift has been installed, please take the time to set it up." && sleep 2s;
+    echo "You'll need to setup your preferred backup method, selected folders and backup storage location!" && sleep 4s;
+    echo "Then, re-run this script!" && sleep 2s
     exit
 fi
 
 # This section of the script will check for automation, folders and files.
 # On first run, it will  create the automation, create folders and move itself into folders.
-if [ $(grep "AutoSHIfT" /etc/anacrontab -c) == 0 ]
-then
-    echo "@weekly 0       AutoSHIfT   ./home/$liuser/AutoSHIfT/AutoSHIfT.sh" >> /etc/anacrontab &&
-    mkdir /home/$liuser/AutoSHIfT && mkdir /home/$liuser/AutoSHIfT/logs &&
-    chown $liuser:$liuser /home/$liuser/AutoSHIfT && chown $liuser:$liuser /home/$liuser/AutoSHIfT/logs &&
-    echo "This is your first time running AutoSHIfT." && sleep 1s;
-    echo "A weekly cron Job has been added to anacrontab, so," && sleep 1s;
-    echo "You'll never have to manually run this script, unless you want to!" && sleep 1s;
-    echo "AutoSHIfT and it's logs will reside in ~>  /home/$liuser/AutoSHIfT" && sleep 1s;
-    echo "You'll receive a persistent notification every time AutoSHIfT runs successfully." && sleep 1s;
-    echo "All backup and update actions are happening in the background, not on screen!" && sleep 1s;
-    echo "You won't see action until the script completes(up to 15 minutes). IT IS NOT FROZEN!!!";
-    echo "Make a habit out of checking the logs ~>  /home/$liuser/AtuoSHIfT/logs";
+if [ "$(grep "AutoSHIfT" /etc/anacrontab -c)" == 0 ]
+    then
+        echo "This is your first time running AutoSHIfT." && sleep 1s;
+        echo "@weekly 0       AutoSHIfT   ./home/$liuser/AutoSHIfT/AutoSHIfT.sh" >> /etc/anacrontab &&
+        echo "A weekly cron Job has been added to anacrontab, so," && sleep 1s;
+        echo "You'll never have to manually run this script, unless you want to!" && sleep 2s;
+        echo "AutoSHIfT and it's logs will reside in ~>  /home/$liuser/AutoSHIfT" && sleep 2s;
+        echo "You'll receive a persistent notification every time AutoSHIfT runs successfully." && sleep 1s;
+        echo "All backup and update actions are happening in the background, not on screen!" && sleep 1s;
+        echo "You won't see action until the script completes(up to 15 minutes). IT IS NOT FROZEN!!!";
+        echo "Make a habit out of checking the logs ~>  /home/$liuser/AtuoSHIfT/logs"
 fi
 
-exec &>> /home/$liuser/AutoSHIfT/logs/$(date +"%m%d%Y").log
-LOCAutoSHIfT=$(updatedb; locate AutoSHIfT.sh | awk '{ if (/.local/ && !seen) { seen = 0 } else print }')
+logfile=$(date +"%m%d%Y-%I%M%p").log
 
-if [ $LOCAutoSHIfT != "/home/$liuser/AutoSHIfT/AutoSHIfT.sh" ]
-then
-    mv $LOCAutoSHIfT /home/$liuser/AutoSHIfT/
+if [ "$(find "$PWD" -type f -name AutoSHIfT.sh  | grep  "/home/$liuser/AutoSHIfT/AutoSHIfT.sh")" == "/home/$liuser/AutoSHIfT/AutoSHIfT.sh" ]
+    then
+        :
+    elif [ "$(find "$PWD" -type f -name AutoSHIfT.sh  | grep  "/home/$liuser/AutoSHIfT/AutoSHIfT.sh")" != "/home/$liuser/AutoSHIfT/AutoSHIfT.sh" ]
+    then
+        mkdir /home/"$liuser"/AutoSHIfT && mkdir /home/"$liuser"/AutoSHIfT/logs &&
+        chown "$liuser":"$liuser" /home/"$liuser"/AutoSHIfT && chown "$liuser":"$liuser" /home/"$liuser"/AutoSHIfT/logs &&
+        mv "$(find "$PWD" -type f -name AutoSHIfT.sh)" /home/"$liuser"/AutoSHIfT/
 fi
 
 # This section performs the backup and system update functions.
 # I've chosen to include flatpak updates here, you can delete and/or replace that command.
-if [ $TSCount -ge $TSList ];
+# the logs will only contain the data from this section
+exec &>> /home/"$liuser"/AutoSHIfT/logs/"$logfile"
+
+if [ "$TSCount" -ge $TSList ];
 then
     echo "0" | timeshift --delete && dnf clean all && timeshift --create --comment AutoSHIfT &&
-    dnf upgrade -y && sudo -u $liuser flatpak update -y
+    dnf upgrade -y && flatpak update -y
 else
-    dnf clean all && timeshift --create --comment AutoSHIfT && dnf upgrade -y && sudo -u $liuser flatpak update -y
+    dnf clean all && timeshift --create --comment AutoSHIfT && dnf upgrade -y && flatpak update -y
 fi
 
-
 # This section creates a persistent notification at the end of each successful run.
-notification="sudo -u $liuser DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $liuser)/bus notify-send -a 'AutoSHIfT' 'System updates succeeded. Reboot Recommended!' 'VIEW THE LOGS! ~/AutoSHIfT/logs/' -u critical"
+notification="sudo --user=$liuser DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u "$liuser")/bus notify-send -a 'AutoSHIfT' 'System updates succeeded. Reboot Recommended!' 'VIEW THE LOGS! ~/AutoSHIfT/logs/' -u critical"
 
 echo "*/1 * * * * $liuser $notification" >> /etc/crontab && sleep 59s &&
 sed -i '/AutoSHIfT/d' /etc/crontab &&
-
-find /home/$liuser/AutoSHIfT/logs -mtime +$D2K -delete
+find /home/"$liuser"/AutoSHIfT/logs -mtime +$D2K -delete
 exit
-
